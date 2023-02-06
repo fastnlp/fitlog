d8d9e7b8b5a132d86a9775c33ae7c912ef69a761
yhcc <poemsmileyh@gmail.com>
2023-02-06 15:57:12 +0800

    0.9.16

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='fitlog',
-    version='0.9.14',
+    version='0.9.16',
     description='fitlog: Log tool for Deep Learning, developed by Fudan FastNLP Team',
     long_description=readme,
     long_description_content_type='text/markdown',
